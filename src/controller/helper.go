0e79e79c3fdce2a278e48a759cd6fac844fd7c39
Yeuoly <admin@srmxy.cn>
2023-06-14 20:11:13 +0800

    bugfix: fucking params bind

--- src/controller/helper.go
+++ src/controller/helper.go
@@ -1,14 +1,26 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/Yeuoly/kisara/src/types"
 	"github.com/gin-gonic/gin"
 )
 
 func BindRequest[T any](r *gin.Context, success func(T)) {
 	var request T
-	err := r.Bind(&request)
+	var err error
+	// check if application/json is set
+
+	context_type := r.GetHeader("Content-Type")
+	if context_type == "application/json" {
+		err = r.BindJSON(&request)
+	} else {
+		err = r.Bind(&request)
+	}
+
 	if err != nil {
+		fmt.Println(request)
 		resp := types.ErrorResponse(-400, err.Error())
 		r.JSON(200, resp)
 		return
